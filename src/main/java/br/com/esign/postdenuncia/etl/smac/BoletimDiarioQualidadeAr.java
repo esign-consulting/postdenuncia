a2d9a37ba7ac6802425865348a94289455e7eaa4
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2020-12-06 11:47:58 +0000

    fix smac datahora

--- src/main/java/br/com/esign/postdenuncia/etl/smac/BoletimDiarioQualidadeAr.java
+++ src/main/java/br/com/esign/postdenuncia/etl/smac/BoletimDiarioQualidadeAr.java
@@ -46,12 +46,12 @@ public class BoletimDiarioQualidadeAr implements FonteMedicao {
 
     private Date obterDatahoraMedicao() {
         Date datahoraMedicao = null;
-        String regex = "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)(\\s)([01]?[0-9]|2[0-3]):[0-5][0-9]";
+        String regex = "(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[012])/((19|20)\\d\\d)";
         Pattern pattern = Pattern.compile(regex);
         Element div = doc.getElementById("titulo");
         Matcher matcher = pattern.matcher(div.html());
         if (matcher.find()) {
-            SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+            SimpleDateFormat parser = new SimpleDateFormat("dd/MM/yyyy");
             try {
                 datahoraMedicao = parser.parse(matcher.group());
             } catch (ParseException e) {
